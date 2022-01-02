cc4e7abdee785227e84299eab4d9e5e5144080bb
Petro Arva <petroarva33@gmail.com>
2022-01-02 18:15:52 +0100

    Added alerts

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,9 +1,11 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 const UserSearch = () => {
   const [text, setText] = useState("");
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
@@ -11,7 +13,7 @@ const UserSearch = () => {
     e.preventDefault();
 
     if (!text) {
-      alert("Please, enter something");
+      setAlert("Please, enter something", "error");
     } else {
       searchUsers(text);
 
