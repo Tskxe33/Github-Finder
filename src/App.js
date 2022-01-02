d666128e985c343570c1ff4691240a2e94119d37
Petro Arva <petroarva33@gmail.com>
2022-01-02 18:14:23 +0100

    Created new user route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import User from "./pages/User";
+
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 
@@ -18,9 +21,11 @@ function App() {
             <Navbar />
 
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/not-found" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
