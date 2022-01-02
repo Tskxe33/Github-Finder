bbdd0a4076aca9478be2e7ddc45ddf1fdd8ccb90
Petro Arva <petroarva33@gmail.com>
2022-01-02 17:17:03 +0100

    Added clearUsers case

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -2,6 +2,8 @@ const githubReducer = (state, action) => {
   switch (action.type) {
     case "GET_USERS":
       return { ...state, users: action.payload, loading: false };
+    case "CLEAR_USERS":
+      return { ...state, users: [] };
     case "SET_LOADING":
       return { ...state, loading: true };
     default:
